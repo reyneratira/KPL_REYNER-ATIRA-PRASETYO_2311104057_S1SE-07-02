5866868cf7f1f6b352daa076d7a880bb1001043e
Reyner Atira Prasetyo <tneon590@gmail.com>
2025-04-10 16:45:51 +0700

    menambahkan class SayaTubeVideo

--- 06_Design_by_Contract_dan_Defensive_Programming/tp/tpmodul6_2311104057/tpmodul6_2311104057/Program.cs
+++ 06_Design_by_Contract_dan_Defensive_Programming/tp/tpmodul6_2311104057/tpmodul6_2311104057/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class SayaTubeVideo
+{
+    private int id;
+    private string title;
+    private int playCount;
+
+    public SayaTubeVideo(string title)
+    {
+        Random rand = new Random(); // menggunakan class bawaan System untuk random
+        this.id = rand.Next(10000, 99999); // ID 5 digit
+        this.title = title;
+        this.playCount = 0;
+    }
+
+    public void IncreasePlayCount(int count)
+    {
+        this.playCount += count;
+    }
+
+    public void PrintVideoDetails()
+    {
+        Console.WriteLine("Video ID       : " + id);
+        Console.WriteLine("Video Title    : " + title);
+        Console.WriteLine("Play Count     : " + playCount);
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        SayaTubeVideo video = new SayaTubeVideo("Tutorial Design By Contract – Reyner Atira Prasetyo");
+        video.IncreasePlayCount(100); // penambahan playCount
+        video.PrintVideoDetails();
+    }
+}
