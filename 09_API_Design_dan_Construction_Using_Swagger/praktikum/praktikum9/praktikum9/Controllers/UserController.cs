f3d9f4d92214834b58bef9d702336edbd06c29a4
Reyner Atira Prasetyo <tneon590@gmail.com>
2025-05-22 19:20:22 +0700

    praktikum modul 9

---
+++ 09_API_Design_dan_Construction_Using_Swagger/praktikum/praktikum9/praktikum9/Controllers/UserController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using praktikum9.Models;
+
+namespace praktikum9.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private static List <User> users = new List<User>
+        {
+            new User{id=1,name="Alisa", email = "alisa@gmail.com"},
+            new User{id=2,name="Bob", email="bob@gmail.com" }
+        };
+
+        [HttpGet]
+        public ActionResult<List<User>> GetUsers()
+        {
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<User> GetUser(int id)
+        {
+            var user = users.FirstOrDefault(u => u.id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
+        [HttpPost]
+        public ActionResult<User> CreateUser(UserDto createUser)
+        {
+            int newId = users.Max(u => u.id) + 1;
+            User newUser = new User
+            {
+                id = newId,
+                name = createUser.name,
+                email = createUser.email
+            };  
+            users.Add(newUser);
+            return Ok(newUser);
+        }
+        [HttpPut("{id}")]
+        public IActionResult UpdateUser(int id, UserDto updateUser)
+        {
+            var user = users.FirstOrDefault(u => u.id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            user.name = updateUser.name;
+            user.email = updateUser.email;
+            return Ok(user);
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteUser(int id)
+        {
+            var user = users.FirstOrDefault(u => u.id == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            users.Remove(user);
+            return NoContent();
+        }
+    }
+}
---
+++ 09_API_Design_dan_Construction_Using_Swagger/praktikum/praktikum9/praktikum9/Models/UserDto.cs
@@ -0,0 +1,8 @@
+﻿namespace praktikum9.Models
+{
+    public class UserDto
+    {
+        public string name { get; set; }
+        public string email { get; set; }
+    }
+}
