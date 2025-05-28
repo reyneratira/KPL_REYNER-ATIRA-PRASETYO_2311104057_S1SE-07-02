a8319f575cab9d0d81de49a5046aa908c52fddf4
Reyner Atira Prasetyo <tneon590@gmail.com>
2025-05-28 10:57:16 +0700

    initialize tp10

---
+++ 10_Library_Construction/praktikum/praktikum10/ClassLibrary1/Utilities.cs
@@ -0,0 +1,15 @@
+﻿namespace UtilityLibrary
+{
+    public static class Utilities
+    {
+        public static bool StartsWithUpper(this string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return false;
+            char ch = str[0];
+            return char.IsUpper(ch);
+
+
+        }
+    }
+}
