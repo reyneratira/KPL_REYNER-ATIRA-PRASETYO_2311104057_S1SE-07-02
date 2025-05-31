9651b754c49717a71bfbd267f0028d628ca2ba29
Reyner Atira Prasetyo <tneon590@gmail.com>
2025-05-31 10:59:39 +0700

    jurnal 10: membuat matematika libraries

---
+++ 10_Library_Construction/jurnal/modul10_2311104057/MatematikaLibraries/Matematika.cs
@@ -0,0 +1,71 @@
+﻿namespace MatematikaLibraries
+{
+    public class Matematika
+    {
+        public int FPB(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+        public int KPK(int a, int b)
+        {
+            return (a * b) / FPB(a, b);
+        }
+
+        public string Turunan(int[] persamaan)
+        {
+            string result = "";
+            int derajat = persamaan.Length - 1;
+
+            for (int i = 0; i < persamaan.Length - 1; i++)
+            {
+                int koef = persamaan[i] * (derajat - i);
+                int pangkat = derajat - i - 1;
+
+                if (koef == 0) continue;
+
+                if (result.Length > 0 && koef > 0)
+                    result += " + ";
+                else if (koef < 0)
+                    result += " - ";
+
+                result += $"{Math.Abs(koef)}";
+
+                if (pangkat > 0)
+                    result += $"x{(pangkat > 1 ? pangkat.ToString() : "")}";
+            }
+
+            return result;
+        }
+
+        public string Integral(int[] persamaan)
+        {
+            string result = "";
+            int derajat = persamaan.Length - 1;
+
+            for (int i = 0; i < persamaan.Length; i++)
+            {
+                int pangkat = derajat - i + 1;
+                double koef = (double)persamaan[i] / pangkat;
+
+                if (persamaan[i] == 0) continue;
+
+                if (result.Length > 0 && koef > 0)
+                    result += " + ";
+                else if (koef < 0)
+                    result += " - ";
+
+                result += $"{Math.Abs(koef)}x{(pangkat > 1 ? pangkat.ToString() : "")}";
+            }
+
+            result += " + C";
+            return result;
+        }
+    }
+}
---
+++ 10_Library_Construction/jurnal/modul10_2311104057/TestMatematika/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using MatematikaLibraries;
+
+class Program
+{
+    static void Main()
+    {
+        Matematika mtk = new Matematika();
+
+        Console.WriteLine("FPB(60, 45) = " + mtk.FPB(60, 45));
+        Console.WriteLine("KPK(12, 8) = " + mtk.KPK(12, 8));
+
+        int[] turunanInput = { 1, 4, -12, 9 };
+        Console.WriteLine("Turunan({1, 4, -12, 9}) = " + mtk.Turunan(turunanInput));
+
+        int[] integralInput = { 4, 6, -12, 9 };
+        Console.WriteLine("Integral({4, 6, -12, 9}) = " + mtk.Integral(integralInput));
+
+        Console.ReadKey();
+    }
+}
