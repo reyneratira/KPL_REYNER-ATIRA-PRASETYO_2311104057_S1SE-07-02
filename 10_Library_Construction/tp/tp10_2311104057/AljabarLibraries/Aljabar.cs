46c80e007d340b04a1c9c4ba15a383939d9e9623
Reyner Atira Prasetyo <tneon590@gmail.com>
2025-05-31 10:50:04 +0700

    tp10: membuat aljabar libraries

---
+++ 10_Library_Construction/tp/tp10_2311104057/AljabarLibraries/Aljabar.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace AljabarLibraries
+{
+    public class Aljabar
+    {
+        // Akar-akar dari persamaan kuadrat
+        public static double[] AkarPersamaanKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+            double c = persamaan[2];
+
+            double D = b * b - 4 * a * c;
+
+            if (D < 0)
+            {
+                // Tidak memiliki akar real
+                return new double[0];
+            }
+
+            double akar1 = (-b + Math.Sqrt(D)) / (2 * a);
+            double akar2 = (-b - Math.Sqrt(D)) / (2 * a);
+
+            return new double[] { akar1, akar2 };
+        }
+
+        // Hasil kuadrat dari persamaan linear
+        public static double[] HasilKuadrat(double[] persamaan)
+        {
+            double a = persamaan[0];
+            double b = persamaan[1];
+
+            double a2 = a * a;
+            double ab2 = 2 * a * b;
+            double b2 = b * b;
+
+            return new double[] { a2, ab2, b2 };
+        }
+    }
+}
---
+++ 10_Library_Construction/tp/tp10_2311104057/AljabarTest/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using AljabarLibraries;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Test AkarPersamaanKuadrat:");
+        double[] persamaanKuadrat = { 1, -3, -10 };
+        double[] akar = Aljabar.AkarPersamaanKuadrat(persamaanKuadrat);
+        Console.WriteLine(string.Join(", ", akar));  // Output: 5, -2
+
+        Console.WriteLine("\nTest HasilKuadrat:");
+        double[] persamaanLinear = { 2, -3 };
+        double[] hasilKuadrat = Aljabar.HasilKuadrat(persamaanLinear);
+        Console.WriteLine(string.Join(", ", hasilKuadrat)); // Output: 4, -12, 9
+    }
+}
