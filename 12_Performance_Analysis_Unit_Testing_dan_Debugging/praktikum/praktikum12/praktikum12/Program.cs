99bb4e22a9348eccf88f9d2a83a8ca30d93b27a0
Reyner Atira Prasetyo <tneon590@gmail.com>
2025-06-02 12:09:46 +0700

    praktikum modul 12

---
+++ 12_Performance_Analysis_Unit_Testing_dan_Debugging/praktikum/praktikum12/praktikum12/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace praktikum12;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string result = DeternmineGrade(90);
+        Console.WriteLine(result);
+    }
+    public static string?DeternmineGrade(int score)
+    {
+        Dictionary<int, string> GradeMap = new Dictionary<int, string>()
+        {
+            { 90, "A" },
+            { 80, "B" },
+            { 70, "C" },
+            { 60, "D" },
+            { 0, "F" }
+        };
+
+        foreach (var kv in GradeMap.OrderByDescending(kv => kv.Key)){
+            if (score >= kv.Key)
+            {
+                return kv.Value;
+            }
+        }
+
+        return null;
+    }
+}
---
+++ 12_Performance_Analysis_Unit_Testing_dan_Debugging/praktikum/praktikum12/unittest12/Test1.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace unittest12
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            String? result = praktikum12.Program.DeternmineGrade(90);
+            Assert.AreEqual("A", result);
+            String? result2 = praktikum12.Program.DeternmineGrade(80);
+            Assert.AreEqual("B", result2);
+            String? result3 = praktikum12.Program.DeternmineGrade(70);
+            Assert.AreEqual("C", result3);
+            String? result4 = praktikum12.Program.DeternmineGrade(60);
+            Assert.AreEqual("D", result4);
+            String? result5 = praktikum12.Program.DeternmineGrade(0);
+            Assert.AreEqual("F", result5);
+
+        }
+    }
+}
