5540194fa14081f17ec0e3a52a07ea5a4593f77b
Reyner Atira Prasetyo <tneon590@gmail.com>
2025-06-02 12:10:50 +0700

    tp12 : membuat windows forms app

---
+++ 12_Performance_Analysis_Unit_Testing_dan_Debugging/tp/tpmodul12_2311104057/tpmodul12_2311104057/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace tpmodul12_2311104057
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.inputTextBox = new System.Windows.Forms.TextBox();
+            this.checkButton = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // inputTextBox
+            // 
+            this.inputTextBox.Location = new System.Drawing.Point(221, 210);
+            this.inputTextBox.Name = "inputTextBox";
+            this.inputTextBox.Size = new System.Drawing.Size(341, 22);
+            this.inputTextBox.TabIndex = 0;
+            // 
+            // checkButton
+            // 
+            this.checkButton.Location = new System.Drawing.Point(318, 251);
+            this.checkButton.Name = "checkButton";
+            this.checkButton.Size = new System.Drawing.Size(130, 23);
+            this.checkButton.TabIndex = 1;
+            this.checkButton.Text = "Cek tanda";
+            this.checkButton.UseVisualStyleBackColor = true;
+            this.checkButton.Click += new System.EventHandler(this.checkButton_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(329, 125);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(0, 16);
+            this.resultLabel.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.checkButton);
+            this.Controls.Add(this.inputTextBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox inputTextBox;
+        private System.Windows.Forms.Button checkButton;
+        private System.Windows.Forms.Label resultLabel;
+    }
+}
+
---
+++ 12_Performance_Analysis_Unit_Testing_dan_Debugging/tp/tpmodul12_2311104057/tpmodul12_2311104057/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tpmodul12_2311104057
+{
+    public partial class Form1 : Form
+    {
+        private string CariTandaBilangan(int a)
+        {
+            if (a < 0)
+                return "Negatif";
+            else if (a > 0)
+                return "Positif";
+            else
+                return "Nol";
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void checkButton_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(inputTextBox.Text, out int number))
+            {
+                resultLabel.Text = CariTandaBilangan(number);
+            }
+            else
+            {
+                try
+                {
+                    // Attempt to parse the input to check for overflow  
+                    Convert.ToInt32(inputTextBox.Text);
+                }
+                catch (OverflowException)
+                {
+                    MessageBox.Show("Bilangan terlalu besar atau kecil untuk tipe data integer.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    resultLabel.Text = string.Empty;
+                    return;
+                }
+
+                catch (FormatException)
+                {
+                    MessageBox.Show("Input tidak valid. Silakan masukkan bilangan bulat.", "Input Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    resultLabel.Text = string.Empty;
+                    return;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
